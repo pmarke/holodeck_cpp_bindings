a207af69cb88eddb869ead0f82a78c158b85365c
Mark Petersen <markiepetersen@gmail.com>
2018-01-26 11:46:42 -0700

    can now get image quickly

--- include/holodeck_cpp_bindings.h
+++ include/holodeck_cpp_bindings.h
@@ -5,133 +5,118 @@
 #include <pybind11/numpy.h>
 #include <pybind11/stl.h>
 #include <pybind11/eigen.h>
-#include <stdint.h>
 
-#ifdef USE_OPENCV
 
-	#include <opencv2/opencv.hpp>
-
-#endif
-
-#include <string>
+#include <opencv2/opencv.hpp>
 
 #include <Eigen/Dense>
+
+// standard libraries
+#include <stdint.h>
+#include <iostream>
+#include <string>
+
+
 
 namespace py = pybind11;
 
 
-
-
 namespace holodeck_bindings {
 
-	typedef Eigen::Matrix<float, 6,1> Eigen_RowVector6f;
+    typedef Eigen::Matrix<float, 6,1> Eigen_RowVector6f;
 
-	typedef Eigen::Matrix<uint8_t,Eigen::Dynamic,Eigen::Dynamic,Eigen::Dynamic> Eigen_MatrixX;
 
-	enum GL_VERSION
-	{
-		OPENGL4=4,
-		OPENGL3 =3
-	};
+    enum GL_VERSION
+    {
+        OPENGL4=4,
+        OPENGL3 =3
+    };
 
-	// The sensor information that you can retrieve
-	enum Sensor
-	{
-	    TERMINAL = 1,
-	    REWARD = 2,
-	    PRIMARY_PLAYER_CAMERA = 3,   // default is 512 x 512 RGBA
-	    CAMERA_SENSOR_ARRAY_2D = 4,  // default is 512 x 512 RGBA
-	    ORIENTATION_SENSOR = 5,
-	    IMU_SENSOR = 6,
-	    JOINT_ROTATION_SENSOR = 7,
-	    RELATIVE_SKELETAL_POSITION_SENSOR = 8,
-	    LOCATION_SENSOR = 9,
-	    VELOCITY_SENSOR = 10
+    // The sensor information that you can retrieve
+    enum Sensor
+    {
+        TERMINAL = 1,
+        REWARD = 2,
+        PRIMARY_PLAYER_CAMERA = 3,   // default is 512 x 512 RGBA
+        CAMERA_SENSOR_ARRAY_2D = 4,  // default is 512 x 512 RGBA
+        ORIENTATION_SENSOR = 5,
+        IMU_SENSOR = 6,
+        JOINT_ROTATION_SENSOR = 7,
+        RELATIVE_SKELETAL_POSITION_SENSOR = 8,
+        LOCATION_SENSOR = 9,
+        VELOCITY_SENSOR = 10
 
-	};
+    };
 
-	class Holodeck
-	{
+    class Holodeck
+    {
 
-	public:
+    public:
 
-		Holodeck();
+        Holodeck();
 
-		~Holodeck();
+        // make the world
+        void make(std::string world_name, GL_VERSION gl_version );
 
-		// make the world
-		void make(std::string world_name, GL_VERSION gl_version );
+        // reset the world and update the state variable
+        void reset();
 
-		// reset the world and update the state variable
-		void reset();
+        // take a step in the world
+        void step(float roll, float pitch, float altitude, float yaw_rate);
+        
+        // return the orientation sensor data
+        void get_orientation_sensor_data();
 
-		// take a step in the world
-		void step(float roll, float pitch, float altitude, float yaw_rate);
-		
-		// return the orientation sensor data
-		Eigen::Matrix3f get_orientation_sensor_data();
+        // return the imu sensor data
+        Eigen_RowVector6f get_imu_sensor_data();
 
-		// return the imu sensor data
-		Eigen_RowVector6f get_imu_sensor_data();
+        // return the location sensor data
+        Eigen::RowVector3f get_location_sensor_data();
 
-		// return the location sensor data
-		Eigen::RowVector3f get_location_sensor_data();
+        // return the velocity sensor data
+        Eigen::RowVector3f get_velocity_sensor_data();
 
-		// return the velocity sensor data
-		Eigen::RowVector3f get_velocity_sensor_data();
+        // returns the primary player camera data
+        Eigen::Ref< Eigen::Matrix3f> get_primary_player_camera(cv::Mat& img);
 
-		#ifndef USE_OPENCV
+            
 
-		// return the camera data
-		uint8_t*** get_primary_player_camera();
-
-		#else
-
-		void get_primary_player_camera(cv::Mat& img);
-			
-		#endif
-		
+        
 
 
 
-	private:
+    private:
 
-		py::scoped_interpreter guard{};// start the py interpreter
-		py::object holodeck_module_;   // used to import Holodeck.py
-		py::object holodeck_;          // Holodeck class. from Holodeck import Holodeck
-		py::object env_;               // Environment object. env=Holodeck.make
-		py::dict state_;               // get the state of the copter
+        py::scoped_interpreter guard{};// start the py interpreter
+        py::object holodeck_module_;   // used to import Holodeck.py
+        py::object holodeck_;          // Holodeck class. from Holodeck import Holodeck
+        py::object env_;               // Environment object. env=Holodeck.make
+        py::dict state_;               // get the state of the copter
 
-		// allocates memory for the primary player camera image
-		void allocatePrimaryPlayerCamera(py::array pyImg);
+        // allocates memory for the primary player camera image
+        void allocatePrimaryPlayerCamera(py::array pyImg);
 
-		// deallocates memory for the primary player camera image
-		void deallocatePrimaryPlayerCamera();
+        // deallocates memory for the primary player camera image
+        void deallocatePrimaryPlayerCamera();
 
-		void getCammeraDimensions(py::array pyImg);
+        void getCammeraDimensions(py::array pyImg);
 
 
-		// primary player camera data
-		int height_;
-		int width_;
-		int depth_;
-		bool allocated_ = false;
+        // primary player camera data
+        int height_;
+        int width_;
+        int depth_;
+        bool allocated_ = false;
 
-		#ifndef USE_OPENCV
-			uint8_t*** img_;           // image data BGRA
-
-		#else
-
-			cv::Mat img_; 
-		#endif
-		
+        
+        
 
 
 
 
 
 
-	};
+    };
 
 
 
--- src/holodeck_cpp_bindings.cpp
+++ src/holodeck_cpp_bindings.cpp
@@ -7,37 +7,29 @@ namespace holodeck_bindings {
 Holodeck::Holodeck() {
 
     // import the py module
-	holodeck_module_ = py::module::import("Holodeck");
-	py::exec("from Holodeck import Holodeck;");
+    holodeck_module_ = py::module::import("Holodeck");
+    py::exec("from Holodeck import Holodeck;");
 
-	// import the holodeck class
-	holodeck_ = holodeck_module_.attr("Holodeck");
+    // import the holodeck class
+    holodeck_ = holodeck_module_.attr("Holodeck");
 
-	
-
-}
-
-Holodeck::~Holodeck(){
-
-    #ifndef USE_OPENCV
-    // delete the dynamic camera array   
-    deallocatePrimaryPlayerCamera();
-    #endif
+    
 
 }
+
 
 void Holodeck::make(std::string world_name, GL_VERSION gl_version = OPENGL4) {
 
-	// Create the world
-	env_ = holodeck_.attr("make")(world_name, static_cast<int>(gl_version));
-	
+    // Create the world
+    env_ = holodeck_.attr("make")(world_name, static_cast<int>(gl_version));
+    
 }
 
 
 void Holodeck::reset() {
 
     // reset the environment and returns [state, terminal, reward, and info]
-	py::tuple result_py = env_.attr("reset")();
+    py::tuple result_py = env_.attr("reset")();
 
     // extract the state
     state_ = result_py[0];
@@ -47,10 +39,10 @@ void Holodeck::reset() {
 void Holodeck::step(float roll, float pitch, float altitude, float yaw_rate) {
 
     // pack commands up
-	Eigen::RowVector4f command(roll,pitch,altitude,yaw_rate);
+    Eigen::RowVector4f command(roll,pitch,altitude,yaw_rate);
 
     // send the commands to the simulator and get states
-	py::tuple result_py = env_.attr("step")(command);
+    py::tuple result_py = env_.attr("step")(command);
 
     // extract state information
     state_ = result_py[0];
@@ -60,15 +52,14 @@ void Holodeck::step(float roll, float pitch, float altitude, float yaw_rate) {
 
 }
 
-
-Eigen::Matrix3f Holodeck::get_orientation_sensor_data() {
+Eigen::Ref< Eigen::Matrix3f> Holodeck::get_orientation_sensor_data() {
 
     // extract data
     py::array pyData = state_.attr("get")(5);
 
-    // return rotation matrix
-    return pyData.cast<Eigen::Matrix3f>();
-
+    // return pointer of data
+    return = pyData.cast<py::EigenDRef<Eigen::Matrix3f>>();
+    
 }
 
 Eigen_RowVector6f Holodeck::get_imu_sensor_data() {
@@ -77,7 +68,7 @@ Eigen_RowVector6f Holodeck::get_imu_sensor_data() {
     py::array pyData = state_.attr("get")(6);
 
     // return the data
-    return pyData.cast<Eigen::Matrix<float, 6,1>>();
+    return pyData.cast<Eigen::Ref<Eigen::Matrix<float, 6,1>>>();
 
 
 }
@@ -88,7 +79,7 @@ Eigen::RowVector3f Holodeck::get_location_sensor_data() {
     py::array pyData = state_.attr("get")(9);
 
     // return the data
-    return pyData.cast<Eigen::RowVector3f>();
+    return pyData.cast<Eigen::Ref<Eigen::RowVector3f>>();
 }
 
 Eigen::RowVector3f Holodeck::get_velocity_sensor_data() {
@@ -97,7 +88,7 @@ Eigen::RowVector3f Holodeck::get_velocity_sensor_data() {
     py::array pyData = state_.attr("get")(10);
 
     // return the data
-    return pyData.cast<Eigen::RowVector3f>();
+    return pyData.cast<Eigen::Ref<Eigen::RowVector3f>>();
 }
 
 void Holodeck::getCammeraDimensions(py::array pyImg) {
@@ -117,155 +108,20 @@ void Holodeck::getCammeraDimensions(py::array pyImg) {
 
 }
 
-#ifdef USE_OPENCV
+
 
 void Holodeck::get_primary_player_camera(cv::Mat& img) {
 
-    // printf("here\n");
-
+    // get camera data
     py::array pyImg = state_.attr("get")(3);
-
-    Eigen_MatrixX test;
-    test(0,0,0) = 1;// = pyImg.cast<Eigen_MatrixX>;
-
-    // py::object arrayInterface = pyImg.attr("data");
-
-    // py::array flat = pyImg.attr("flat");
-
-    // uint8_t test[] = flat.cast<py::array_t<uint8_t>>();
-
-    // void* test = flat.ptr();
-
-
-    // py::tuple data = arrayInterface.attr("get")("data");
-
-    // py::int memaddr = data[0];
-
-    // py::print(memaddr);
-
-    // int test =  data[0].cast<int>();
-
-
-
-    // py::print(test);
-    // get camera image dimensions
+    
+    // get dimensions of image
     getCammeraDimensions(pyImg);
-
-
-    // uint8_t tempImg[height_][width_][depth_]; 
-
-    // uint8_t* temp =  &pyImg.attr("item")(0,0,0).cast<uint8_t>();
-
-    // uint8_t* temImg = &pyImg;
-
-    // unpack the image
-    // for(int i = 0; i < width_; i++) {
-
-    //     for (int j = 0; j < height_; j++) {
-
-    //         for (int d = 0; d < depth_; d++) {
-
-    //             tempImg[i][j][d] = pyImg.attr("item")(i,j,d).cast<uint8_t>();
-                
-    //         }
-         
-    //     }
-    // }
-
-    // convert it to cv::Mat object
-    img_ = cv::Mat(height_,width_, CV_8UC4,pyImg.ptr());
-
-    img = img_.clone();
-
-    cv::imshow("img", img);
-    cv::waitKey(0);
-
-    // delete test;
+    
+    // convert numpy ndarray to cv::Mat object
+    img = cv::Mat(height_,width_, CV_8UC4,pyImg.mutable_data());
 
 }
-
-#endif
-
-
-#ifndef USE_OPENCV
-uint8_t*** Holodeck::get_primary_player_camera()
-{
-    // extract the image from state_
-    py::array pyImg = state_.attr("get")(3);
-
-
-    allocatePrimaryPlayerCamera(pyImg);
-
-   
-
-
-    for(int i = 0; i < width_; i++) {
-
-        for (int j = 0; j < height_; j++) {
-
-            for (int d = 0; d < depth_; d++) {
-
-                img_[i][j][d] = pyImg.attr("item")(i,j,d).cast<uint8_t>();
-                
-            }
-         
-        }
-    }
-
- 
-
-
-
-    return img_;
-
-}
-
-
-void Holodeck::allocatePrimaryPlayerCamera(py::array pyImg)
-{
-    // if not allocated
-    if (!allocated_)
-    {
-
-        getCammeraDimensions( pyImg);
-
-        // allocate the memeory
-        img_ = new uint8_t**[height_];
-        for(int i = 0; i < width_; i++) {
-
-            img_[i] = new uint8_t*[width_];
-
-            for(int j = 0; j < height_; j++) {
-
-                img_[i][j] = new uint8_t[depth_];
-            }
-        }
-
-    }
-}
-
-void Holodeck::deallocatePrimaryPlayerCamera()
-{
-
-    // deallocate the memory if it has been allocated
-
-    if(allocated_) {
-
-        for(int i = 0; i < height_; i++) {
-
-            for(int j = 0; j < width_; j++) {
-                
-                delete [] img_[i][j];
-            }
-
-            delete [] img_[i];
-
-        }
-
-        delete [] img_;
-    }
-}
-#endif
 
 
 
